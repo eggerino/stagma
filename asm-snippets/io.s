6bd58ae7c770d6d3837abc3b307c80192a9c757a
eggerino <eggensberger.armin@googlemail.com>
2024-02-04 20:03:49 +0100

    added args preparation asm snippet

--- asm-snippets/io.s
+++ asm-snippets/io.s
@@ -103,6 +103,53 @@ section .text
 
     _start:
 
+        ; args
+        ; r12: argc
+        ; r13: if
+        ; r14: ptr
+
+        mov r12, [rsp]              ; get argc from the stack
+        dec r12                     ; ignore first argument (program name)
+        mov r13, 0                  ; i = 0
+
+    .args_loop:
+        cmp r13, r12                ; if (i == argc)
+        je .args_loop_done          ;     break;
+
+        mov r14, r12                ; the current argument to push is argc 
+        inc r14                     ; + 1 items above the stack pointer
+        imul r14, 8                 ; convert count to size
+        add r14, rsp                ; offset by stack pointer to get pointer to argument
+
+        mov rdi, [r14]              ; place argument in rdi for atoll
+        mov rsi, 256                ; use null termination for stop criteria in atoll
+        call atoll
+
+        push rax                    ; push the parsed integer argument to the stack
+        inc r13                     ; ++i
+
+        jmp .args_loop              ; continue the loop
+
+    .args_loop_done:
+        push r12                    ; push argc itself on the stack
+
+        ; actual program starts here
+        
+        ; pop
+        pop rdi
+    
+        ; print
+        pop rdi
+        call print
+
+        ; print
+        pop rdi
+        call print
+
+        ; print
+        pop rdi
+        call print
+
         ; input
         call input
         push rax
